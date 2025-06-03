f1a1f18539367ab96aa9df3a6aa13523e3916b39
csslick <digitone@naver.com>
2025-06-03 11:05:31 +0900

    header layout

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,7 @@
 import './style.css'
-import { diaryData } from './diary-data'
+// import { diaryData } from './diary-data'
 import { router } from './router'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <header class='bg-[#007AFF] text-white flex w-full justify-between items-center px-[20px] py-[12px]'>
-      <h1 class='text-[28px]'>Diary</h1>
-      <button class='text-lg'>완료</button>
-    </header>
-    <main class='p-4'>
-      여기에 페이지 내용이 들어갑니다.
-    </main>
-  </div>
-`
 
 // 페이지 로드시 router 실행
 window.addEventListener('load', () => {
--- src/router.ts
+++ src/router.ts
@@ -11,16 +11,34 @@ export function router() {
     return
   }
   
+  const getLayout = (title: string, page: string) => {
+    return `
+      <div>
+        <header class='bg-[#007AFF] text-white flex w-full justify-between items-center px-[20px] py-[12px]'>
+          <h1 class='text-[28px]'>${title}</h1>
+          ${page === 'detail' ? `<button class='text-lg'>완료</button>` : ''}
+          ${page === 'write' ? `<button class='text-lg'>완료</button>` : ''}
+        </header>
+        <main class='p-4'>
+          여기에 페이지 내용이 들어갑니다.
+        </main>
+      </div>
+    `
+  }
+  
   // 경로에 따라 페이지 랜더링
   const hash = window.location.hash;
   console.log('Current path:', hash);
   if (hash === '' || hash === '#/') {
+    app.innerHTML = getLayout('Diary', 'home');
     Home(app);
   }
   else if (hash === '#/write') {
+    app.innerHTML = getLayout('쓰기', 'write');
     Write(app);
   }
   else if (hash === ('#/detail')) {
+    app.innerHTML = getLayout('보기', 'detail');
     Detail(app);
   } else {
     // 기본 경로로 리다이렉트
