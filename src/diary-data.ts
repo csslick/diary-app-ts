9e82a6270fa103ae0bf1a4fbba5d141d8949fa53
csslick <digitone@naver.com>
2025-06-06 18:30:42 +0900

    detail page

--- src/diary-data.ts
+++ src/diary-data.ts
@@ -7,7 +7,7 @@ export interface Diary {
 
 export const diaryData: Diary[] = [
   {
-    id: crypto.randomUUID(), // 고유 ID 생성
+    id: '1', // 고유 ID 생성
     date: '2023-10-01', // 날짜
     content: '오늘은 날씨가 맑고 기분이 좋았다. 아침에 산책을 하고, 오후에는 친구와 커피를 마셨다.' // 일기 내용
   },
--- src/pages/Detail.ts
+++ src/pages/Detail.ts
@@ -1,12 +1,28 @@
-export default function Detail(container: HTMLElement) {
-  const main = container.querySelector<HTMLElement>('main');
+import { diaryData } from "../diary-data";
 
+export default function Detail(container: HTMLElement) {
+  // qurey string에서 id 추출
+  const query = new URLSearchParams(window.location.hash.split('?')[1]);
+  const id = query.get('id');
+  console.log('Detail page id:', id);
+  // diaryData에서 해당 id의 일기 찾기
+  const diary = diaryData.find(d => d.id === id);
+
+  const main = container.querySelector<HTMLElement>('main');
   if (!main) {
     throw new Error('Main element not found');
     return
   }
 
   main.innerHTML = `
-    <h1>Detail</h1>
+    <div class="diary-item bg-gray-200 py-[18px] px-[15px] rounded-lg mb-5">
+      <p>${diary?.content}</p>
+      <footer class="flex justify-between items-center mt-2">
+        <time class="text-sm text-gray-500">${diary?.date}</time>
+        <button class="delete-button">
+          <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="#DC3545" d="M19 4h-3.5l-1-1h-5l-1 1H5v2h14M6 19a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V7H6z"/></svg>
+        </button>
+      </footer>
+    </div>
   `;
 }
